509ecec0d6d9602bc4147f36966a6f9e53fc4e84
Luis Freire <frebermu9@gmail.com>
2022-11-01 17:20:32 -0500

    Creating SuperarketBag Class

---
+++ src/org/luisf/model/SupermarketBag.java
@@ -0,0 +1,32 @@
+package org.luisf.model;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class SupermarketBag<T> implements Iterable<T>{
+
+    private List<T> products;
+    private int max;
+
+    public SupermarketBag(int max) {
+        this.max = max;
+        this.products = new ArrayList<>();
+    }
+
+    public void addProduct(T product){
+        if(products.size() <= max)
+            this.products.add(product);
+        else
+            throw new RuntimeException("Bag Full");
+    }
+
+    public List<T> getProducts(){
+        return products;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return this.products.iterator();
+    }
+}
